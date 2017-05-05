653179f80d51f5ef1c47cac948e8b2ee60f9381d
odeclasonarres <odeclasonarres@gmail.com>
2017-05-05 16:43:02 +0200

    Avanzando ejercicios

--- BP3/Codigo/scheduled-clause.c
+++ BP3/Codigo/scheduled-clause.c
@@ -6,25 +6,44 @@
    #define omp_get_thread_num() 0
 #endif
 
-int main(int argc, char **argv) { 
+
+int main(int argc, char **argv) {
   int i, n=200,chunk,a[n],suma=0;
-   if(argc < 3)     {
-      fprintf(stderr,"\nFalta iteraciones o chunk \n");
-      exit(-1);
-     }
-   n = atoi(argv[1]); if (n>200) n=200; chunk = atoi(argv[2]); 
+  if(argc < 3){
+    fprintf(stderr,"\nFalta iteraciones o chunk \n");
+    exit(-1);
+  }
+  n = atoi(argv[1]); if (n>200) n=200; chunk = atoi(argv[2]);
+  printf("%s\n", omp_in_parallel());
+  for (i=0; i<n; i++) a[i] = i;
 
-   for (i=0; i<n; i++)       a[i] = i; 
- 
-   #pragma omp parallel for firstprivate(suma) \
+  #pragma omp parallel for firstprivate(suma) \
             lastprivate(suma) schedule(dynamic,chunk)
-   for (i=0; i<n; i++)
-   {   suma = suma + a[i];
-       printf(" thread %d suma a[%d]=%d suma=%d \n",
-             omp_get_thread_num(),i,a[i],suma);
-   }  
-  
-   printf("Fuera de 'parallel for' suma=%d\n",suma); 
+   for (i=0; i<n; i++){
 
+     suma = suma + a[i];
+     printf(" thread %d suma a[%d]=%d suma=%d \n", omp_get_thread_num(),i,a[i],suma);
+     printf("Dyn-var: %s\n", omp_get_dynamic());
+     printf("%s\n", omp_in_parallel());
+     //printf("nthreads-var: %s\n", omp_get_max_threads());
+     //printf("thread-limit-var: %s\n", omp_get_thread_limit() );
+     //printf("un-sched-var: %s\n",omp_get_schedule(&kind, &modifier) );
+     //printf("num_threads: %s\n",omp_get_num_threads());
+     //printf("num_procs: %s\n",omp_get_num_procs() );
+     //printf("in_parallel: %s\n", omp_in_parallel());
+   }
+
+
+
+   printf("Fuera de 'parallel for' suma=%d\n",suma);
+   //printf("Dyn-var: %s\n", omp_get_dynamic());
+   //omp_set_dynamic(1);
+   //printf("Dyn-var2: %s\n", omp_get_dynamic());
+   //printf("nthreads-var: %s\n", omp_get_max_threads());
+   //printf("thread-limit-var: %s\n", omp_get_thread_limit() );
+   //printf("un-sched-var: %s\n",omp_get_schedule(&kind, &modifier) );
+   //printf("num_threads: %s\n",omp_get_num_threads());
+   //printf("num_procs: %s\n",omp_get_num_procs() );
+   //printf("in_parallel: %s\n",omp_in_parallel());
    return(0);
 }
